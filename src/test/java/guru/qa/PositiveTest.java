7a14df1e2501acf66f08ae4c9e40266e1aa84f97
Castryulka <castryulka@gmail.com>
2021-10-10 20:14:01 +0300

    added some positive and negative tests

---
+++ src/test/java/guru/qa/PositiveTest.java
@@ -0,0 +1,31 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PositiveTest {
+    @Test
+    void firstPositiveTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void secondPositiveTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void thirdTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void fourthPositiveTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void fifthPositiveTest() {
+        Assertions.assertTrue(true);
+    }
+}
